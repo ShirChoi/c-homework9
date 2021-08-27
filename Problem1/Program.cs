3e7d7c48c9561a87256f62aedadf81420b0bc7d1
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-27 12:25:05 +0500

    first commit

---
+++ Problem1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            Converter converter = new Converter(11.3280, 13.3093, 0.1525);
+
+            double somoni = Read("somoni");
+
+            Console.WriteLine($"{somoni} somoni = {converter.SomonToDollar(somoni)} dollars");
+            Console.WriteLine($"{somoni} somoni = {converter.SomonToEuro(somoni)} euros");
+            Console.WriteLine($"{somoni} somoni = {converter.SomonToRouble(somoni)} roubles");
+
+            double dollar = Read("dollar");
+            System.Console.WriteLine($"{dollar} dollars = {converter.DollarToSomon(dollar)} somoni");
+
+            double euro = Read("euro");
+            System.Console.WriteLine($"{euro} euros = {converter.EuroToSomon(euro)} somoni");
+
+            double rouble = Read("rouble");
+            System.Console.WriteLine($"{rouble} roubles = {converter.RoubleToSomon(rouble)} somoni");
+        } 
+
+        static double Read(string varName) {
+            System.Console.Write($"{varName}: ");
+
+            return double.Parse(Console.ReadLine());
+        }
+    }
+
+    class Converter {
+        private double _usd;
+        private double _eur;
+        private double _rub;
+
+        public Converter(double usd, double eur, double rub) {
+            _usd = usd;
+            _eur = eur;
+            _rub = rub;
+        }
+
+        public double SomonToDollar(in double somoni) {
+            return somoni / _usd;
+        }
+
+        public double SomonToEuro(in double somoni) {
+            return somoni / _eur;
+        }
+
+        public double SomonToRouble(in double somoni) {
+            return somoni / _rub;
+        }
+
+        public double DollarToSomon(in double dollar) {
+            return dollar * _usd;
+        }
+
+        public double EuroToSomon(in double euro) {
+            return euro * _eur;
+        }
+
+        public double RoubleToSomon(in double rouble) {
+            return rouble * _rub;
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Problem2 {
+    class Program {
+        static void Main(string[] args) {
+            Employee employee1 = new Employee("Eshmatov", "Toshmat", EmployeePosition.Junior);
+            employee1.PrintInfo();
+
+            System.Console.WriteLine();
+
+            Employee employee2 = new Employee("Aminov", "Khusrav", EmployeePosition.Middle);
+            employee2.PrintInfo();
+
+            System.Console.WriteLine();
+
+            Employee employee3 = new Employee("Jeff", "Bezos", EmployeePosition.TeamLead);
+            employee3.PrintInfo();
+        }
+    }
+
+    public enum EmployeePosition {
+        Junior,
+        Middle,
+        TeamLead,
+    }
+
+    class Employee {
+        public string Name {get; private set;}
+        public string Surname {get; private set;}
+        public EmployeePosition Position {get; private set;}
+
+        public Employee(string name, string surname, EmployeePosition position) {
+            Name = name;
+            Surname = surname;
+            Position = position;
+        }
+
+        public void PrintInfo() {
+            Console.WriteLine($"name:\t\t{Name}");            
+            Console.WriteLine($"surname:\t{Surname}");   
+            PrintPositionInfo();
+            PrintSalaryInfo();     
+        }
+
+        private void PrintPositionInfo() {
+            string position = Position switch {
+                EmployeePosition.Junior => "Junior",
+                EmployeePosition.Middle => "Middle",
+                EmployeePosition.TeamLead => "TeamLead",
+                _ => "",
+            };
+
+            Console.WriteLine($"position:\t{position}");
+        }
+
+        private void PrintSalaryInfo() {
+            double salary = Position switch {
+                EmployeePosition.Junior => 1000,
+                EmployeePosition.Middle => 2500,
+                EmployeePosition.TeamLead => 3200,
+                _ => 0,
+            };
+
+            double tax = salary * (13 + 1) / 100;
+
+            Console.WriteLine($"slary:\t\t{salary}");
+            Console.WriteLine($"tax from slary:\t{tax}");
+
+        }
+    }
+}
